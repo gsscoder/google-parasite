54ff689c4c86a64f50d4e41afade4b9a1bfaae67
Giacomo Stelluti Scala <gsscoder@gmail.com>
2021-08-12 07:27:06 +0200

    Used render and BeautifulSoup directly

--- google_parasite/core/service.py
+++ google_parasite/core/service.py
@@ -1,11 +1,13 @@
 import urllib.parse
 from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 
 def query(subject):
     session = HTMLSession()
     request = session.get(f'https://www.google.com/search?q={urllib.parse.quote(subject)}')
-    return request.html
+    request.html.render(timeout=2)
+    return BeautifulSoup(request.html.raw_html, 'html.parser')
 
 def do_math(expression):
-    html = query(expression)
-    return float(html.find('#cwos')[0].text)
+    soup = query(expression)
+    return float(soup.find_all('span', id='cwos')[0].text)
