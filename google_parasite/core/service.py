046506e72a0e001105e912a3edca69d2b5e8a676
Giacomo Stelluti Scala <gsscoder@gmail.com>
2021-08-12 14:19:21 +0200

    Added get_summary function

--- google_parasite/core/service.py
+++ google_parasite/core/service.py
@@ -2,12 +2,19 @@ import urllib.parse
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
-def query(subject):
+def query(text):
     session = HTMLSession()
-    request = session.get(f'https://www.google.com/search?q={urllib.parse.quote(subject)}')
-    request.html.render(timeout=2)
+    request = session.get(f'https://www.google.com/search?q={urllib.parse.quote(text)}')
+    request.html.render(timeout=5)
     return BeautifulSoup(request.html.raw_html, 'html.parser')
 
 def do_math(expression):
     soup = query(expression)
     return float(soup.find_all('span', id='cwos')[0].text)
+
+def get_summary(subject):
+    items = []
+    soup = query(subject)
+    for li in soup.find_all('ol')[1]:
+       items.append(li.text.strip())
+    return items
